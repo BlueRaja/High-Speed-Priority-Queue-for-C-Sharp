a907b894d2aa58dd1ecffff3ab8a281454f98cab
BlueRaja <BlueRaja.admin@gmail.com>
2016-04-15 01:49:38 -0500

    Make InsertionIndex and QueueIndex internal, so users can't edit them by mistake

--- Priority Queue/FastPriorityQueueNode.cs
+++ Priority Queue/FastPriorityQueueNode.cs
@@ -3,20 +3,19 @@
     public class FastPriorityQueueNode
     {
         /// <summary>
-        /// The Priority to insert this node at.  Must be set BEFORE adding a node to the queue
+        /// The Priority to insert this node at.  Must be set BEFORE adding a node to the queue.
+        /// Should not be manually edited once the node has been enqueued - use queue.UpdatePriority() instead
         /// </summary>
         public double Priority { get; set; }
 
         /// <summary>
-        /// <b>Used by the priority queue - do not edit this value.</b>
         /// Represents the order the node was inserted in
         /// </summary>
-        public long InsertionIndex { get; set; }
+        public long InsertionIndex { get; internal set; }
 
         /// <summary>
-        /// <b>Used by the priority queue - do not edit this value.</b>
         /// Represents the current position in the queue
         /// </summary>
-        public int QueueIndex { get; set; }
+        public int QueueIndex { get; internal set; }
     }
 }
--- Priority Queue/Properties/AssemblyInfo.cs
+++ Priority Queue/Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
+
+[assembly: InternalsVisibleTo("Priority Queue Tests")]
