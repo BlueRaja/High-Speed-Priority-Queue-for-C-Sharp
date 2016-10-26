bfcd71f61362add9fc3769508fdcffb8e5a2744d
isimongauz <isimongauz@gmcs.ru>
2016-10-26 18:08:44 +0300

    .Net standart

---
+++ Priority Queue Net Standart/FastPriorityQueueNode.cs
@@ -0,0 +1,16 @@
+﻿namespace Priority_Queue
+{
+    public class FastPriorityQueueNode
+    {
+        /// <summary>
+        /// The Priority to insert this node at.  Must be set BEFORE adding a node to the queue (ideally just once, in the node's constructor).
+        /// Should not be manually edited once the node has been enqueued - use queue.UpdatePriority() instead
+        /// </summary>
+        public float Priority { get; protected internal set; }
+
+        /// <summary>
+        /// Represents the current position in the queue
+        /// </summary>
+        public int QueueIndex { get; internal set; }
+    }
+}
