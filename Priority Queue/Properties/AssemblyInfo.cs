cdd7e25ec73baeed408f1b46df27fd627e186b7c
BlueRaja <BlueRaja.admin@gmail.com>
2021-02-26 16:45:25 -0800

    Added strong name to assembly

--- Priority Queue/Properties/AssemblyInfo.cs
+++ Priority Queue/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3eee6b54-af8a-494b-9121-3d46ed09a58b")]
 
-[assembly: InternalsVisibleToAttribute("Priority Queue Tests")]
+[assembly: InternalsVisibleTo("Priority Queue Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100794a91e4cf03eda7c3406cbc7247bdae9f498905b805173bbd3bb97613cb2afa69311aef40119245618d08a9c84edae6b545795f2b8dc81a9ed2f70598e341b4a67d9e96fe23dfa80f61db1dd47cb2d58992c2cd3dc8d6f4744aeda94c21c018d5e63e1cc9ff5ded1030e9b092315e00c04391429bc311e4f2597114cee1efae")]
 
 // Version information for an assembly consists of the following four values:
 //
